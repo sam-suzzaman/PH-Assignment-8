9e22bce790c286136254e1303ab2ad68a6e654e5
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-03-26 09:28:39 +0600

    adding fakeData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import fakeData from './utilities/fakeData';
 
 function App() {
+  const [data,setData]=useState(fakeData);
+  console.log(data);
+  const {name,image}=data[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{name}</h2>
+      <img src={image} alt="" />
     </div>
   );
 }
---
+++ src/utilities/fakeData.js
@@ -0,0 +1,90 @@
+import img1 from './img/cake-1.jpg';
+import img2 from './img/cake-2.jpg';
+import img3 from './img/cake-3.jpg';
+import img4 from './img/cake-4.jpg';
+import img5 from './img/cake-5.jpg';
+import img6 from './img/cake-6.jpg';
+import img7 from './img/cake-7.jpg';
+import img8 from './img/cake-8.jpg';
+import img9 from './img/cake-9.jpg';
+import img10 from './img/cake-10.jpg';
+import img11 from './img/cake-11.jpg';
+import img12 from './img/cake-12.jpg';
+
+const fakeData = [
+   {
+       id:1,
+       name:'cake-1',
+       price:10,
+       image:img1
+   },
+   {
+       id:2,
+       name:'cake-2',
+       price:20,
+       image:img2
+   },
+   {
+       id:3,
+       name:'cake-3',
+       price:30,
+       image:img3
+   },
+   {
+       id:4,
+       name:'cake-4',
+       price:40,
+       image:img4
+   },
+   {
+       id:5,
+       name:'cake-5',
+       price:70,
+       image:img5
+   },
+   {
+       id:6,
+       name:'cake-6',
+       price:90,
+       image:img6
+   },
+   {
+       id:7,
+       name:'cake-7',
+       price:100,
+       image:img7
+   },
+   {
+       id:8,
+       name:'cake-8',
+       price:200,
+       image:img8
+   },
+   {
+       id:9,
+       name:'cake-9',
+       price:440,
+       image:img9
+   },
+   {
+       id:10,
+       name:'cake-10',
+       price:300,
+       image:img10
+   },
+   {
+       id:11,
+       name:'cake-1',
+       price:270,
+       image:img11
+   },
+   {
+       id:12,
+       name:'cake-12',
+       price:370,
+       image:img12
+   },
+
+  ]
+
+  export default fakeData;
