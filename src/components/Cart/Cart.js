b460b1a1bc292f28f2f42196bc313f46f22ebaa0
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-03-26 20:31:06 +0600

    make site responsive

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { AiOutlineDelete } from 'react-icons/ai';
 import './cart.css';
 
 const Cart=(props)=>{
     const{name,image}=props.cart;
     return(
         <div className="cart">
-            <div className="left">
-                <div className="img-container">
-                    <img src={image} alt="" />
-                </div>
-                <h3>{name}</h3>
+            <div className="img-container">
+                <img src={image} alt="" />
             </div>
-            <button><AiOutlineDelete/></button>
+            <h3>{name}</h3>
         </div>
     )
 }
